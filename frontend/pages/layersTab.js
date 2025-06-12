2597a2dd02f7b0c739cd28072df9110b6ef209cc
fahdjerbi <fahd.jerbi@fshst.u-tunis.tn>
2025-06-12 17:42:13 +0100

    add "layers card structure and buttons"

--- frontend/pages/layersTab.js
+++ frontend/pages/layersTab.js
@@ -47,6 +47,50 @@ function showLayers() {
   });
 }
 
-showLayers();
+// showLayers();
+
+// function showLayers() {
+//   layersContainer.innerHTML = "";
+
+//   if (layers.length === 0) {
+//     layersContainer.innerHTML = "<p>No Lyaers</p>";
+//     return;
+//   }
+
+//   layers.map((layer) => {
+//     // layer Card
+//     const card = document.createElement("div");
+//     card.className = "card-body";
+//     const title = document.createElement("h5");
+//     title.className = "card-title";
+//     title.textContent = layer.name;
+
+//     const btn = document.createElement("a");
+//     btn.className = "btn btn-danger";
+//     btn.textContent = "Delete";
+
+//     // switcher
+//     const switcher = document.createElement("div");
+//     switcher.className = "form-check form-switch";
+//     const switcherInput = document.createElement("input");
+//     switcherInput.className = "form-check-input";
+//     switcherInput.type = "checkbox";
+//     switcherInput.role = "switch";
+//     switcherInput.id = "switchCheckChecked";
+//     switcherInput.checked = true;
+//     const switcherLabel = document.createElement("label");
+//     switcherLabel.className = "form-check-label";
+//     switcherLabel.htmlFor = "switchCheckChecked";
+
+//     switcher.appendChild(switcherInput);
+//     switcher.appendChild(switcherLabel);
+
+//     card.appendChild(title);
+//     card.appendChild(btn);
+//     card.appendChild(switcher);
+
+//     layersContainer.appendChild(card);
+//   });
+// }
 
 // add a zoom to layer funcionality once clicked on a layer card
